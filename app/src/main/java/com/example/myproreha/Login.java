82f7d0b20a7bea86d46cb6c93372e22917a21cb2
Vivian9908 <Vivian.petkovsek@hotmail.de>
2023-07-05 15:27:09 +0200

    aktuell

--- app/src/main/java/com/example/myproreha/Login.java
+++ app/src/main/java/com/example/myproreha/Login.java
@@ -3,6 +3,11 @@ package com.example.myproreha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText phone = findViewById(R.id.phone);
+        final EditText password = findViewById(R.id.password);
+        final Button loginBtn = findViewById(R.id.loginBtn):
+        final TextView registerNow = findViewById(R.id.registerNowBtn);
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String phoneTxt = phone.getText().toString();
+                final  String passwordTxt = password.getText().toString();
+
+                if (phoneTxt.isEmpty()|| passwordTxt.isEmpty()){
+
+                    Toast.makeText(Login.this, "Pleas enter your mobile or password", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+
+                }
+            }
+        });
+
+
     }
 }
