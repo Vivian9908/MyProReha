4194f1b4daf8c29b4a2e1a4d9b152bc1cc1c73f8
Vivian9908 <Vivian.petkovsek@hotmail.de>
2023-07-06 14:42:02 +0200

    get and set Data

--- app/src/main/java/com/example/myproreha/DataClass.java
+++ app/src/main/java/com/example/myproreha/DataClass.java
@@ -7,10 +7,33 @@ public class DataClass {
     private String dataDuration;
     private String dataNotes;
 
+    public String getDataDate() {
+        return dataDate;
+    }
+
+    public String getDataTherapie() {
+        return dataTherapie;
+    }
+
+    public String getDataDuration() {
+        return dataDuration;
+    }
+
+    public String getDataNotes() {
+        return dataNotes;
+    }
+
     public DataClass(String dataDate, String dataTherapie, String dataDuration, String dataNotes) {
         this.dataDate = dataDate;
         this.dataTherapie = dataTherapie;
         this.dataDuration = dataDuration;
         this.dataNotes = dataNotes;
+
+
     }
+
+    public DataClass(){
+
+    }
+
 }
---
+++ app/src/main/java/com/example/myproreha/MyAdapter.java
@@ -0,0 +1,74 @@
+package com.example.myproreha;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import android.content.Context;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    private Context context;
+    private List<DataClass> dataList;
+
+    public MyAdapter(Context context, List<DataClass> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+       holder.recTitle.setText(dataList.get(position).getDataTherapie());
+       holder.recDate.setText(dataList.get(position).getDataDate());
+       holder.recDuration.setText(dataList.get(position).getDataDuration());
+       holder.recNotes.setText(dataList.get(position).getDataNotes());
+
+       holder.recCard.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(context, )
+           }
+       });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
+class MyViewHolder extends RecyclerView.ViewHolder{
+
+    TextView recTitle, recDate, recDuration, recNotes;
+    CardView recCard;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        recTitle = itemView.findViewById(R.id.recTitle);
+        recDate = itemView.findViewById(R.id.recDate);
+        recDuration = itemView.findViewById(R.id.recDuration);
+        recNotes = itemView.findViewById(R.id.recNotes);
+
+
+
+    }
+}
