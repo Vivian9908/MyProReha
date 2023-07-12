073300a8b1242cf7c047843c53f3565154b5b95e
Vivian9908 <Vivian.petkovsek@hotmail.de>
2023-07-12 19:26:46 +0200

    Update MyProfile.java

--- app/src/main/java/com/example/myproreha/MyProfile.java
+++ app/src/main/java/com/example/myproreha/MyProfile.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -34,7 +35,6 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,7 +46,6 @@ public class MyProfile extends AppCompatActivity {
     TextView profileName, profileMail, sum;
     FloatingActionButton deleteBtn;
     private ActivityResultLauncher<Intent> launcher;
-
 
 
     private ImageView profilePic;
@@ -97,6 +96,7 @@ public class MyProfile extends AppCompatActivity {
                     profileMail.setText(dataEmail);
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Handle Fehler beim Datenbankzugriff
@@ -108,9 +108,9 @@ public class MyProfile extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 int sumDuration = 0; // Initialize sumDuration variable
                 for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
-                    String dataDuration = itemSnapshot.child( "dataDuration").getValue(String.class);
+                    String dataDuration = itemSnapshot.child("dataDuration").getValue(String.class);
                     Log.e("UpdateActivity", "Failed to load therapy data" + dataDuration);
-                   int duration = Integer.valueOf(dataDuration); // Convert dataDuration to int
+                    int duration = Integer.valueOf(dataDuration); // Convert dataDuration to int
                     sumDuration += duration; // Add duration to sumDuration
                 }
                 sum.setText(String.valueOf(sumDuration) + " Minuten"); // Set the sumDuration as text in the sum TextView
@@ -125,28 +125,49 @@ public class MyProfile extends AppCompatActivity {
         deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users/"+ GlobalVariables.currentUser);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MyProfile.this);
+                builder.setTitle("Bestätigung");
+                builder.setMessage("Möchten Sie das Konto wirklich löschen?");
 
-                reference.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                builder.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onSuccess(Void unused) {
+                    public void onClick(DialogInterface dialog, int which) {
+                        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users/" + GlobalVariables.currentUser);
 
-                        FirebaseStorage storage = FirebaseStorage.getInstance();
-                        StorageReference fileReference = storage.getReference("users/"+ GlobalVariables.currentUser);
-
-                        fileReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        reference.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
-                                // Deletion from Firebase Storage is successful
-                                Toast.makeText(MyProfile.this, "Deleted", Toast.LENGTH_SHORT).show();
-                                finish();
+
+                                FirebaseStorage storage = FirebaseStorage.getInstance();
+                                StorageReference fileReference = storage.getReference("users/" + GlobalVariables.currentUser);
+
+                                fileReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        // Deletion from Firebase Storage is successful
+                                        Toast.makeText(MyProfile.this, "Gelöscht", Toast.LENGTH_SHORT).show();
+                                        finish();
+                                    }
+                                });
                             }
                         });
+                        startActivity(new Intent(getApplicationContext(), Login.class));
                     }
                 });
-                startActivity(new Intent(getApplicationContext(), Login.class));
+
+                builder.setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Abbrechen, wenn der Benutzer "Nein" wählt
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
+
     }
 
     private void choosePicture() {
@@ -161,36 +182,46 @@ public class MyProfile extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
             imageUri = data.getData();
             profilePic.setImageURI(imageUri);
-            uploadPicture();
+            uploadPicture(imageUri);
 
         }
     }
 
-    private void uploadPicture() {
-
+    private void uploadPicture(Uri imageUri) {
+        // Erstelle einen Dateinamen für das Bild basierend auf dem aktuellen Zeitstempel
         AlertDialog.Builder builder = new AlertDialog.Builder(MyProfile.this);
         builder.setView(R.layout.progress_layout);
 
-final String randomKey = UUID.randomUUID().toString();
-StorageReference riversRef = storageReference.child("images/" + randomKey);
+        final String randomKey = UUID.randomUUID().toString();
+        // Erhalte eine Referenz auf den Firebase Storage
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
 
-riversRef.putFile(imageUri)
-        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Snackbar.make(findViewById(android.R.id.content), "Image uploaded", Snackbar.LENGTH_LONG).show();
-            }
-        })
-        .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MyProfile.this, "Failed Image Upload", Toast.LENGTH_SHORT).show();
-            }
-        });
+        // Erstelle eine Referenz auf den Speicherort, an dem das Bild hochgeladen werden soll
+        StorageReference imageRef = storageRef.child("images/" + randomKey);
 
+        // Lade das Bild in den Firebase Storage hoch
+        imageRef.putFile(imageUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    // Der Upload war erfolgreich
+                    // Erhalte die herunterladbare URL des Bildes
+                    imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                        String imageUrl = uri.toString();
+                        // Hier kannst du die URL des Bildes weiterverarbeiten (z. B. speichern oder anzeigen)
+                        // ...
+
+                        // Beispiel: Zeige die herunterladbare URL in einem Toast an
+                        Toast.makeText(getApplicationContext(), "Bild hochgeladen: " + imageUrl, Toast.LENGTH_SHORT).show();
+                    });
+                })
+                .addOnFailureListener(e -> {
+                    // Ein Fehler ist aufgetreten
+                    // Zeige eine Fehlermeldung an
+                    Toast.makeText(getApplicationContext(), "Fehler beim Hochladen des Bildes.", Toast.LENGTH_SHORT).show();
+                });
     }
+
 
 }
