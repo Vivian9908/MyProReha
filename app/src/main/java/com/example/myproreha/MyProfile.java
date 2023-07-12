b494e212ebf5a14bc5c74378cea88d7ae5979d36
Vivian9908 <Vivian.petkovsek@hotmail.de>
2023-07-12 12:06:26 +0200

    my Profile

--- app/src/main/java/com/example/myproreha/MyProfile.java
+++ app/src/main/java/com/example/myproreha/MyProfile.java
@@ -3,12 +3,22 @@ package com.example.myproreha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MyProfile extends AppCompatActivity {
+
+    TextView profileName, profileMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        profileName= findViewById(R.id.profileName);
+        profileMail=findViewById(R.id.profileMail);
+
+
+
+
     }
 }
