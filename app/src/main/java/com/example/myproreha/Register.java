cc47f4266fde0cebccd33a531934efe9b34475e2
NoahGora <81632511+NoahGora@users.noreply.github.com>
2023-07-13 12:17:03 +0200

    clean code Register

--- app/src/main/java/com/example/myproreha/Register.java
+++ app/src/main/java/com/example/myproreha/Register.java
@@ -3,37 +3,26 @@ package com.example.myproreha;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.common.util.UidVerifier;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 
 public class Register extends AppCompatActivity{
     private FirebaseAuth mAuth;
     private EditText fullname, email;
     private EditText password;
-    private Button registerBtn;
-    private TextView loginNow;
     private FirebaseUser user;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
@@ -47,30 +36,18 @@ public class Register extends AppCompatActivity{
         fullname = findViewById(R.id.fullname);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-        registerBtn= findViewById(R.id.registerBtn);
-        loginNow = findViewById(R.id.loginNow);
+        Button registerBtn = findViewById(R.id.registerBtn);
+        TextView loginNow = findViewById(R.id.loginNow);
 
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Register();
+        registerBtn.setOnClickListener(v -> SignUp());
 
-            }
-
-        });
-
-        loginNow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Register.this, Login.class));
-            }
-        });
+        loginNow.setOnClickListener(v -> startActivity(new Intent(Register.this, Login.class)));
 
 
 
     }
 
-    private void Register() {
+    private void SignUp() {
         String fullnam = fullname.getText().toString();
         String emai = email.getText().toString();
         String pass = password.getText().toString();
@@ -89,27 +66,24 @@ public class Register extends AppCompatActivity{
             password.setError("Passwort darf nicht fehlen!");
         }
         else{
-            mAuth.createUserWithEmailAndPassword(emai, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        Toast.makeText(Register.this, "User registeres succesfully",Toast.LENGTH_SHORT ).show();
-                        startActivity(new Intent(Register.this,MainActivity.class));
-                        user = FirebaseAuth.getInstance().getCurrentUser();
-                        reference = FirebaseDatabase.getInstance().getReference("users");
+            mAuth.createUserWithEmailAndPassword(emai, pass).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    Toast.makeText(Register.this, "Erfolgreich registriert",Toast.LENGTH_SHORT ).show();
+                    startActivity(new Intent(Register.this,MainActivity.class));
+                    user = FirebaseAuth.getInstance().getCurrentUser();
+                    reference = FirebaseDatabase.getInstance().getReference("users");
 
-                        GlobalVariables.currentUser = user.getUid().toString();
-                        Log.d("currentUser", "user: "+ GlobalVariables.currentUser);
+                    GlobalVariables.currentUser = user.getUid();
+                    Log.d("currentUser", "user: "+ GlobalVariables.currentUser);
 
 
 
 
-                        DataClass helperClass = new DataClass(fullnam, emai, pass );
-                        reference.child(GlobalVariables.currentUser).setValue(helperClass);
-                    }
-                    else{
-                        Toast.makeText(Register.this, "Registration Failed"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+                    DataClass helperClass = new DataClass(fullnam, emai, pass );
+                    reference.child(GlobalVariables.currentUser).setValue(helperClass);
+                }
+                else{
+                    Toast.makeText(Register.this, "Registrierung fehlgeschlagen "+ Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
         }
